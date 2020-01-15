33138eafb76dc7a15804d0c504c901ca7cada957
ZhengAu <598359997@qq.com>
2020-01-16 00:03:32 +0800

    add message type

---
+++ Pub_SubPattern/03/MessageType.ts
@@ -0,0 +1,19 @@
+/**
+ * 定义的模块信息。任意字符串，不重复就行
+ */
+class MessageType {
+
+	/**
+	 * 更新信息1
+	 */
+	public static UPDATE_MESSAGE: string = 'UPDATE_MESSAGE';
+
+	/**
+	 * 登陆信息
+	 */
+	public static UPDATE_LOGIN_INFO: string = 'UPDATE_LOGIN_INFO';
+}
+
+export {
+	MessageType
+}
