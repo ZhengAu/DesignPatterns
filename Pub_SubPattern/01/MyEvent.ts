a0413fc91334c2306c0a5d67d7c9388ce2958e02
ZhengAu <598359997@qq.com>
2020-01-15 23:06:24 +0800

    防止数组坍塌

--- Pub_SubPattern/01/MyEvent.ts
+++ Pub_SubPattern/01/MyEvent.ts
@@ -30,10 +30,15 @@ export class MyEvent {
 		if (!funs || !funs.length) {
 			return;
 		}
-		let params = param;
-		funs.forEach(item => {
-			item.apply(this, params);
-		});
+		for (let i = 0; i < funs.length; i++) {
+			let item = funs[i];
+			if (!item) {
+				funs.splice(i, 1);//触发的时候再执行删除
+				i--;
+				continue;
+			}
+			item.apply(this, param);
+		}
 	}
 
 	public removeEvent(key: string, func: Function): void {
@@ -41,9 +46,11 @@ export class MyEvent {
 		if (!funs || !funs.length) {
 			return;
 		}
-		for (let i = 0, fun; fun = funs[i++];) {
-			if (fun == func) {
-				funs.splice(i, 1);
+		for (let i = 0; i < funs.length; i++) {
+			let item = funs[i];
+			if (item == func) {
+				funs[i] = null;//防止数组坍塌，先置空
+				break;
 			}
 		}
 		if (!funs.length) {
@@ -72,9 +79,12 @@ export class MyEvent {
 	}
 }
 
+console.log(`========================test1===========================`);
+
 let func1 = function (price: number) {
 	console.log(`price = `, price);
 }
+
 let func2 = function (price: number, num: number = 100) {
 	console.log(`price = ${price}, num = ${num}.`);
 }
@@ -83,8 +93,6 @@ MyEvent.ins().addEvent('myEvent', func1);
 MyEvent.ins().addEvent('myEvent2', func2);
 MyEvent.ins().triggerEvent('myEvent', 1200);
 MyEvent.ins().triggerEvent('myEvent2', 1200, 100000);
-
-console.log(`===================================================`);
 
 let postChange = function () {
 	console.log(`postChange...`);
@@ -95,3 +103,42 @@ let postChangeKey = GlobalFunc.getQualifiedClassName(postChange);
 MyEvent.ins().addEvent(postChangeKey, func1);
 MyEvent.ins().addEvent(postChangeKey, func2);
 MyEvent.ins().triggerEvent(postChangeKey, 8989898989);
+
+console.log(`========================test2===========================`);
+
+let fn1 = function () {
+	console.log(`fn1...`);
+	MyEvent.ins().removeEvent(keyPoint, fn1);
+}
+
+let fn2 = function () {
+	console.log(`fn2...`);
+}
+
+let fn3 = function () {
+	console.log(`fn3...`);
+}
+
+let fn4 = function () {
+	console.log(`fn4...`);
+}
+
+let fn5 = function () {
+	console.log(`fn5...`);
+}
+
+let keyPoint = GlobalFunc.getQualifiedClassName(fn1);
+MyEvent.ins().addEvent(keyPoint, fn1);
+MyEvent.ins().addEvent(keyPoint, fn1);
+MyEvent.ins().addEvent(keyPoint, fn1);
+MyEvent.ins().addEvent(keyPoint, fn1);
+MyEvent.ins().addEvent(keyPoint, fn1);
+MyEvent.ins().addEvent(keyPoint, fn2);
+MyEvent.ins().addEvent(keyPoint, fn3);
+MyEvent.ins().addEvent(keyPoint, fn4);
+MyEvent.ins().addEvent(keyPoint, fn5);
+
+MyEvent.ins().triggerEvent(keyPoint);
+console.log(MyEvent.ins().events);
+MyEvent.ins().triggerEvent(keyPoint);
+console.log(MyEvent.ins().events);
