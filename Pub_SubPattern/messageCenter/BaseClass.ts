a32fa8cb336767264224e383abad0cb731ca16d0
ZhengAu <598359997@qq.com>
2020-01-16 23:44:17 +0800

    add baseClass

---
+++ Pub_SubPattern/messageCenter/BaseClass.ts
@@ -0,0 +1,16 @@
+/**
+ * 基础类
+ */
+export class BaseClass {
+	public constructor() {
+
+	}
+
+	public static ins(): any {
+		let Class: any = this;
+		if (!Class._instance) {
+			Class._instance = new Class();
+		}
+		return Class._instance;
+	}
+}
