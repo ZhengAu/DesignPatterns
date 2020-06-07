2a32b6ba7884290bcd2653a9041337771d42abdd
ZhengAu <598359997@qq.com>
2020-06-07 23:10:56 +0800

    add getQualifiedFunctionName

--- Pub_SubPattern/messageCenter/GlobalFunc.ts
+++ Pub_SubPattern/messageCenter/GlobalFunc.ts
@@ -38,6 +38,7 @@ export class GlobalFunc extends BaseClass {
 	}
 
 	/**
+	 * 传入函数表达式不适合
 	 * 返回对象的完全限定类名
 	 * @param value 需要完全限定类名称的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型（如number)和类对象
 	 * @returns 包含完全限定类名称的字符串。
@@ -69,5 +70,33 @@ export class GlobalFunc extends BaseClass {
 			return;
 		}
 		return func && func.name ? func.name : 'anonymous';
+	}
+
+	public getQualifiedFunctionName2(func: Function): string {
+		if (!func || typeof func !== 'function') {
+			console.error(`传入参数不对 --- ${func}`);
+			return typeof func;
+		}
+		if (func.name) {
+			return func.name;
+		}
+
+		let prototype = func.prototype
+			? func.prototype
+			: Object.getPrototypeOf(func);
+		if (prototype && prototype.constructor.name) {
+			return prototype.constructor.name;
+		}
+
+		let constructorString = prototype.constructor.toString().trim();
+		let index = constructorString.indexOf('(');
+		let className = constructorString.substring(9, index);
+
+		Object.defineProperty(prototype, '__class__', {
+			value: className,
+			enumerable: false,
+			writable: true,
+		});
+		return className ? className : 'anonymous';
 	}
 }
